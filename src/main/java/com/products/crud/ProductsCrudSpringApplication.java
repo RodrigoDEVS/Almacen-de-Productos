e2bf4221ae26b564c4e97b40ed0a2aea5ff7c665
RodrigoDEVS <rodrigo.gallego@outlook.es>
2022-03-25 14:01:45 -0500

    Initial commit

---
+++ src/main/java/com/products/crud/ProductsCrudSpringApplication.java
@@ -0,0 +1,13 @@
+package com.products.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsCrudSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsCrudSpringApplication.class, args);
+	}
+
+}
