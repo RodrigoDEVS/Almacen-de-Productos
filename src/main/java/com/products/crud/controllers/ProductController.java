9632bff76f2b18d9cfda080951e549bb2f2f08d8
RodrigoDEVS <rodrigo.gallego@outlook.es>
2022-03-25 17:20:49 -0500

    creado ProductController

---
+++ src/main/java/com/products/crud/controllers/ProductController.java
@@ -0,0 +1,21 @@
+package com.products.crud.controllers;
+
+import com.products.crud.models.dao.IProductDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProductController {
+
+    @Autowired
+    private IProductDao productDao;
+
+    @GetMapping("/listar")
+    public String listar(Model model){
+        model.addAttribute("titulo", "Listado de Productos");
+        model.addAttribute("Productos", productDao.findAll());
+        return "listar";
+    }
+}
